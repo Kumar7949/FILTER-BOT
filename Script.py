84badd6de02968032a6b65b7527057db4d1a3c74
Kumar7949 <ganik7949@gmail.com>
2025-04-14 08:15:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -244,10 +244,9 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : @KAB_Movies {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ :@KAB_Movies {file_name}
 
-<b>⚙️ sɪᴢᴇ : {file_size}</b>"@Devi_Movies"" 
-📥 JOIN ➼ @TeluguTvSerials_Shows
+<b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
