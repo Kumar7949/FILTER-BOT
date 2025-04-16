d8346782c17b38621e7b26e507d4b62ca15410e4
Kumar7949 <ganik7949@gmail.com>
2025-04-16 10:40:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,10 +125,10 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://pocolinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '0efb284354ff597da0a048c103de88715860ada4')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Open_Shortner_Links/10') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
+SHORTLINK_API = environ.get('SHORTLINK_API', '')
+TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
